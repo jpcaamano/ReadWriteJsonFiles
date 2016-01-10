bb5a8acc9fc57a44d719f62670d5c4dae8a10e48
Juan Pablo Caamano E <juan.caamano@outlook.com>
2016-01-09 21:01:15 -0500

    Proyecto 0

---
+++ ReadWriteJSONFiles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteJSONFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione la Opcion");
+            Console.WriteLine("1.- Emitir Factura");
+            Console.WriteLine("2.- Agregar Cliente");
+            Console.WriteLine("3.- Editar Cliente");
+            Console.WriteLine("4.- Agregar Producto");
+
+        }
+    }
+}
